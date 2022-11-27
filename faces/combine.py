2e1a33647b8499122f3ae1f0706059d11b67ce25
EVERYTHINGING <mikeparisi@gmail.com>
2022-11-27 14:54:22 -0700

    using moviepy

--- faces/combine.py
+++ faces/combine.py
@@ -1,13 +1,38 @@
 from moviepy.editor import VideoFileClip, CompositeVideoClip, vfx
+import random
+
 bg = VideoFileClip("bg.mp4")
 flowers = VideoFileClip("flowers.mp4")
 flowers = vfx.mask_color(flowers)
 
 layers = [bg]
 
-for i in range(1,5):
-  layer = flowers.resize(1/i)
-  layer.set_position((500, 500))
+for i in range(1,30):
+  layer = flowers.resize((random.random()/2)+0.5)
+  
+  if random.random() > 0.5:
+    layer = layer.fx(vfx.mirror_x)
+
+  if random.random() > 0.5:
+    layer = layer.fx(vfx.mirror_y)
+
+  layer = layer.rotate(random.random()*360)
+
+  distx = (random.random()*100)+300
+  distx *= 1 if (random.random() > 0.5) else -1
+  posx = (bg.w/2)
+  posx += distx
+  posx -= (layer.w/2)
+
+  disty = (random.random()*100)+300
+  disty *= 1 if (random.random() > 0.5) else -1
+  posy = (bg.h/2)
+  posy += disty
+  posy -= (layer.h/2)
+
+  print(posx, posy)
+  
+  layer = layer.set_position((posx, posy))
   layers.append(layer)
 
 
